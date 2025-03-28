ae6b0c4dc9ff6b0aeea82ba2f7eb363349e5c45d
unknown <ivanalbano9@gmail.com>
2025-03-28 14:36:24 +0200

    minor changes

--- home-suite/tailwind.config.ts
+++ home-suite/tailwind.config.ts
@@ -18,57 +18,57 @@ module.exports = withUt({
 			},
 		},
 		extend: {
-			// colors: {
-			// 	primary: {
-			// 		500: "#624CF5",
-			// 		50: " #F6F8FD",
-			// 		DEFAULT: "#624CF5",
-			// 		foreground: "hsl(var(--primary-foreground))",
-			// 	},
-			// 	coral: {
-			// 		500: "#15BF59",
-			// 	},
+			colors: {
+				primary: {
+					500: "#624CF5",
+					50: " #F6F8FD",
+					DEFAULT: "#624CF5",
+					foreground: "hsl(var(--primary-foreground))",
+				},
+				coral: {
+					500: "#15BF59",
+				},
 
-			// 	grey: {
-			// 		600: "#545454", // Subdued - color name in figma
-			// 		500: "#757575",
-			// 		400: "#AFAFAF", // Disabled - color name in figma
-			// 		50: "#F6F6F6", // White Grey - color name in figma
-			// 	},
-			// 	black: "#000000",
-			// 	white: "#FFFFFF",
-			// 	border: "hsl(var(--border))",
-			// 	input: "hsl(var(--input))",
-			// 	ring: "hsl(var(--ring))",
-			// 	foreground: "hsl(var(--foreground))",
-			// 	secondary: {
-			// 		DEFAULT: "hsl(var(--secondary))",
-			// 		foreground: "hsl(var(--secondary-foreground))",
-			// 	},
-			// 	destructive: {
-			// 		DEFAULT: "hsl(var(--destructive))",
-			// 		foreground: "hsl(var(--destructive-foreground))",
-			// 	},
-			// 	muted: {
-			// 		DEFAULT: "hsl(var(--muted))",
-			// 		foreground: "hsl(var(--muted-foreground))",
-			// 	},
-			// 	accent: {
-			// 		DEFAULT: "hsl(var(--accent))",
-			// 		foreground: "hsl(var(--accent-foreground))",
-			// 	},
-			// 	popover: {
-			// 		DEFAULT: "hsl(var(--popover))",
-			// 		foreground: "hsl(var(--popover-foreground))",
-			// 	},
-			// 	card: {
-			// 		DEFAULT: "hsl(var(--card))",
-			// 		foreground: "hsl(var(--card-foreground))",
-			// 	},
-			// },
-			// fontFamily: {
-			// 	poppins: ["var(--font-poppins)"],
-			// },
+				grey: {
+					600: "#545454", // Subdued - color name in figma
+					500: "#757575",
+					400: "#AFAFAF", // Disabled - color name in figma
+					50: "#F6F6F6", // White Grey - color name in figma
+				},
+				black: "#000000",
+				white: "#FFFFFF",
+				border: "hsl(var(--border))",
+				input: "hsl(var(--input))",
+				ring: "hsl(var(--ring))",
+				foreground: "hsl(var(--foreground))",
+				secondary: {
+					DEFAULT: "hsl(var(--secondary))",
+					foreground: "hsl(var(--secondary-foreground))",
+				},
+				destructive: {
+					DEFAULT: "hsl(var(--destructive))",
+					foreground: "hsl(var(--destructive-foreground))",
+				},
+				muted: {
+					DEFAULT: "hsl(var(--muted))",
+					foreground: "hsl(var(--muted-foreground))",
+				},
+				accent: {
+					DEFAULT: "hsl(var(--accent))",
+					foreground: "hsl(var(--accent-foreground))",
+				},
+				popover: {
+					DEFAULT: "hsl(var(--popover))",
+					foreground: "hsl(var(--popover-foreground))",
+				},
+				card: {
+					DEFAULT: "hsl(var(--card))",
+					foreground: "hsl(var(--card-foreground))",
+				},
+			},
+			fontFamily: {
+				poppins: ["var(--font-poppins)"],
+			},
 			backgroundImage: {
 				"shapes-pattern": "url('/assets/images/animated-shape.svg')",
 				"hero-img": "url('/assets/images/hero_1.png')",
